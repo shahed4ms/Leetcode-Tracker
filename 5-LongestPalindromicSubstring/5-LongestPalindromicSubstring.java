e4234382f57257ce22315ebbc009156c2cf8f9cd
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:41:56 +0530

    Create 5-LongestPalindromicSubstring

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -0,0 +1,30 @@
+// Last updated: 11/08/2026, 11:41:56
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        String res = "";
+
+        for(int i = 0;i<n;i++){
+            // Odd length palindrome
+            int st = i,end = i;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            String temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
+
+            // Even length palindrome
+            st = i;
+            end = i+1;
+            while(st>=0&&end<n&&s.charAt(st)==s.charAt(end)){
+                st--;
+                end++;
+            }
+            temp = s.substring(st+1,end);
+            if(temp.length()>res.length())res=temp;
+        }
+
+        return res;
+    }
+}
