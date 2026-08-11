f855b9f40b616ce091d41772943d49adeea1f5c4
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:38:24 +0530

    Create 204-CountPrimes

---
+++ 204-CountPrimes/204-CountPrimes.java
@@ -0,0 +1,20 @@
+// Last updated: 11/08/2026, 11:38:24
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        int cnt=0;
+        for (int i = 2; i < n; i++) {
+            prime[i] = true;
+        }
+
+        for (int i = 2;i < n; i++) {
+            if (prime[i]) {
+                cnt++;
+                for (int j = i * 2; j < n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+        return cnt;
+    }
+}
