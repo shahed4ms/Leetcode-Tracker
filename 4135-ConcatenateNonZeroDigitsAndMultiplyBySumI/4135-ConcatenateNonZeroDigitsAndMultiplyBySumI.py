b93a8c0fc3e3cee2d4f9b595d4589ee94b63c0bd
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:36:07 +0530

    Create 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI/4135-ConcatenateNonZeroDigitsAndMultiplyBySumI.py
@@ -0,0 +1,11 @@
+# Last updated: 11/08/2026, 11:36:06
+class Solution:
+    def sumAndMultiply(self, n: int) -> int:
+        x = 0
+        sum = 0
+        for c in str(n):
+            d = int(c)
+            sum += d
+            if d > 0:
+                x = x * 10 + d
+        return x * sum
