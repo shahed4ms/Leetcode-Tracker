c88ce4c083b918f5c37f1e66450a1f86283c4211
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:40:01 +0530

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.java
@@ -0,0 +1,18 @@
+// Last updated: 11/08/2026, 11:40:01
+class Solution {
+    public int jump(int[] nums) {
+        int near = 0, far = 0, jumps = 0;
+
+        while (far < nums.length - 1) {
+            int farthest = 0;
+            for (int i = near; i <= far; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            near = far + 1;
+            far = farthest;
+            jumps++;
+        }
+
+        return jumps;        
+    }
+}
