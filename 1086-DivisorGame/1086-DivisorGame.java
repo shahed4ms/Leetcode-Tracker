b1659f0cd122445ff2f4af6bcc62b9f26e437a73
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:37:08 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.java
@@ -0,0 +1,10 @@
+// Last updated: 11/08/2026, 11:37:08
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
