f0aa13c72ee381fec5eca2d00d69641baf930fa2
Mohammad Shahed S <shahedsharfi@gmail.com>
2026-08-11 11:36:31 +0530

    Create 3405-CountTheNumberOfSpecialCharactersIi

---
+++ 3405-CountTheNumberOfSpecialCharactersIi/3405-CountTheNumberOfSpecialCharactersIi.java
@@ -0,0 +1,21 @@
+// Last updated: 11/08/2026, 11:36:31
+class Solution {
+    public int numberOfSpecialChars(String word) {
+        boolean[][] A = new boolean[2][27];
+
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int idx = ch & 31;
+            int Case = (ch >> 5) & 1;
+
+            A[Case][idx] = Case == 0 || !A[0][idx];
+        }
+
+        int res = 0;
+        for (int i = 1; i < 27; i++)
+            if (A[0][i] && A[1][i])
+                res++;
+
+        return res;
+    }
+}
